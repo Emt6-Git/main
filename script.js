0a8268829f44aaf0e0f72bf82c34476242e8397b
Emt6 <ereker-2033@livoniapublicschools.org>
2026-08-04 14:50:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let "nav-bar" = document.getElementById("nav");
+
+nav-bar.innerHTML = "<a href='index.html'><img src='/images/logo+text.png'></a><a href='https://scratch.mit.edu/users/emt6' class='nav-content' target='_blank' rel='noopener noreferrer'>Scratch Profile</a><a href='about.html' class='nav-content'>About</a>'"
